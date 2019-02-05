891116ccccb1983c76a006f3ac6c722466960f29
Azumi SUZUKI <azumi.suzuki@tier4.jp>
2019-02-05 11:12:59 +0900

    add test of decision_maker

--- ros/src/computing/planning/decision/packages/decision_maker/test/src/test_node_state_drive.cpp
+++ ros/src/computing/planning/decision/packages/decision_maker/test/src/test_node_state_drive.cpp
@@ -14,121 +14,192 @@
  * limitations under the License.
  */
 
-#include <gtest/gtest.h>
 #include <ros/ros.h>
+#include <gtest/gtest.h>
 
-#include "amathutils_lib/amathutils.hpp"
 #include "decision_maker_node.hpp"
+#include "amathutils_lib/amathutils.hpp"
 
-#include "test_class.hpp"
-
-namespace decision_maker {
+namespace decision_maker
+{
 
 class TestSuite : public ::testing::Test {
 public:
-  TestSuite() {}
-  ~TestSuite() {}
-
-  TestClass test_obj_;
+  TestSuite(){}
+  ~TestSuite(){}
 
 protected:
-  virtual void SetUp() {
+  virtual void SetUp()
+  {
     int argc;
-    char **argv;
-    test_obj_.dmn = new DecisionMakerNode(argc, argv);
+    char** argv;
+    dmn = new DecisionMakerNode(argc, argv);
   };
-  virtual void TearDown() { delete test_obj_.dmn; };
+  virtual void TearDown()
+  {
+    delete dmn;
+  };
+
+  void createFinalWaypoints()
+  {
+    autoware_msgs::Lane final_lane;
+    for (int idx = 0; idx < 100; idx++)
+    {
+      static autoware_msgs::Waypoint wp;
+      wp.gid = idx;
+      wp.lid = idx;
+      wp.pose.pose.position.x = 0.0 + (double)idx;
+      wp.pose.pose.position.y = 0.0;
+      wp.pose.pose.position.z = 0.0;
+      wp.twist.twist.linear.x = 5.0;
+      wp.twist.twist.angular.z = 0.0;
+
+      wp.wpstate.steering_state = autoware_msgs::WaypointState::STR_STRAIGHT;
+      wp.wpstate.stop_state = autoware_msgs::WaypointState::NULLSTATE;
+      wp.wpstate.event_state = autoware_msgs::WaypointState::TYPE_EVENT_NULL;
+
+      final_lane.waypoints.push_back(wp);
+    }
+
+    dmn->current_status_.finalwaypoints = final_lane;
+  }
+
+  void setSteeringState(int index, uint8_t state)
+  {
+    dmn->current_status_.finalwaypoints.waypoints.at(index).wpstate.steering_state = state;
+  }
+
+  void setEventState(int index, uint8_t state)
+  {
+    dmn->current_status_.finalwaypoints.waypoints.at(index).wpstate.event_state = state;
+  }
+
+  void setStopState(int index, uint8_t state)
+  {
+    dmn->current_status_.finalwaypoints.waypoints.at(index).wpstate.stop_state = state;
+  }
+
+  void setCurrentPose(double x, double y, double yaw)
+  {
+    geometry_msgs::Pose current_pose;
+    current_pose.position.x = x;
+    current_pose.position.y = y;
+    tf::Quaternion quaternion = tf::createQuaternionFromRPY(0, 0, yaw);
+    quaternionTFToMsg(quaternion, current_pose.orientation);
+
+    dmn->current_status_.pose = current_pose;
+  }
+
+  void setCurrentVelocity(double vel)
+  {
+    dmn->current_status_.velocity = vel;
+  }
+
+  DecisionMakerNode* dmn;
+  uint8_t getSteeringStateFromWaypoint() { return dmn->getSteeringStateFromWaypoint(); }
+  uint8_t getEventStateFromWaypoint() { return dmn->getEventStateFromWaypoint(); }
+  std::pair<uint8_t, int> getStopSignStateFromWaypoint() { return dmn->getStopSignStateFromWaypoint(); }
 };
 
-TEST_F(TestSuite, getSteeringStateFromWaypoint) {
-	test_obj_.createFinalWaypoints();
-  ASSERT_EQ(test_obj_.getSteeringStateFromWaypoint(),
-            autoware_msgs::WaypointState::STR_STRAIGHT)
-      << "Could not get the expected state"
-      << "It should be " << autoware_msgs::WaypointState::STR_STRAIGHT;
+TEST_F(TestSuite, getSteeringStateFromWaypoint)
+{
+  createFinalWaypoints();
+  ASSERT_EQ(getSteeringStateFromWaypoint(), autoware_msgs::WaypointState::STR_STRAIGHT)
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::STR_STRAIGHT;
 
-  test_obj_.setSteeringState(20, autoware_msgs::WaypointState::STR_LEFT);
-  ASSERT_EQ(test_obj_.getSteeringStateFromWaypoint(),
-            autoware_msgs::WaypointState::STR_LEFT)
-      << "Could not get the expected state"
-      << "It should be " << autoware_msgs::WaypointState::STR_LEFT;
+  setSteeringState(20, autoware_msgs::WaypointState::STR_LEFT);
+  ASSERT_EQ(getSteeringStateFromWaypoint(), autoware_msgs::WaypointState::STR_LEFT)
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::STR_LEFT;
 
-  test_obj_.setSteeringState(20, autoware_msgs::WaypointState::STR_RIGHT);
-  ASSERT_EQ(test_obj_.getSteeringStateFromWaypoint(),
-            autoware_msgs::WaypointState::STR_RIGHT)
-      << "Could not get the expected state"
-      << "It should be " << autoware_msgs::WaypointState::STR_RIGHT;
+  setSteeringState(20, autoware_msgs::WaypointState::STR_RIGHT);
+  ASSERT_EQ(getSteeringStateFromWaypoint(), autoware_msgs::WaypointState::STR_RIGHT)
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::STR_RIGHT;
 }
 
-TEST_F(TestSuite, getEventStateFromWaypoint) {
-	test_obj_.setCurrentPose(0, 0, 0);
+TEST_F(TestSuite, getEventStateFromWaypoint)
+{
+  setCurrentPose(0, 0, 0);
 
-	test_obj_.createFinalWaypoints();
-  ASSERT_EQ(test_obj_.getEventStateFromWaypoint(),
-            autoware_msgs::WaypointState::TYPE_EVENT_NULL)
-      << "Could not get the expected state"
-      << "It should be " << autoware_msgs::WaypointState::TYPE_EVENT_NULL;
+  createFinalWaypoints();
+  ASSERT_EQ(getEventStateFromWaypoint(), autoware_msgs::WaypointState::TYPE_EVENT_NULL)
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::TYPE_EVENT_NULL;
 
-  test_obj_.setEventState(20, autoware_msgs::WaypointState::TYPE_EVENT_GOAL);
-  ASSERT_EQ(test_obj_.getEventStateFromWaypoint(),
-            autoware_msgs::WaypointState::TYPE_EVENT_GOAL)
-      << "Could not get the expected state"
-      << "It should be " << autoware_msgs::WaypointState::TYPE_EVENT_GOAL;
+  setEventState(20, autoware_msgs::WaypointState::TYPE_EVENT_GOAL);
+  ASSERT_EQ(getEventStateFromWaypoint(), autoware_msgs::WaypointState::TYPE_EVENT_GOAL)
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::TYPE_EVENT_GOAL;
 
-  test_obj_.setEventState(20, autoware_msgs::WaypointState::TYPE_EVENT_MIDDLE_GOAL);
-  ASSERT_EQ(test_obj_.getEventStateFromWaypoint(),
-            autoware_msgs::WaypointState::TYPE_EVENT_MIDDLE_GOAL)
-      << "Could not get the expected state"
-      << "It should be "
-      << autoware_msgs::WaypointState::TYPE_EVENT_MIDDLE_GOAL;
+  setEventState(20, autoware_msgs::WaypointState::TYPE_EVENT_MIDDLE_GOAL);
+  ASSERT_EQ(getEventStateFromWaypoint(), autoware_msgs::WaypointState::TYPE_EVENT_MIDDLE_GOAL)
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::TYPE_EVENT_MIDDLE_GOAL;
 
-  test_obj_.setEventState(20, autoware_msgs::WaypointState::TYPE_EVENT_POSITION_STOP);
-  ASSERT_EQ(test_obj_.getEventStateFromWaypoint(),
-            autoware_msgs::WaypointState::TYPE_EVENT_POSITION_STOP)
-      << "Could not get the expected state"
-      << "It should be "
-      << autoware_msgs::WaypointState::TYPE_EVENT_POSITION_STOP;
+  setEventState(20, autoware_msgs::WaypointState::TYPE_EVENT_POSITION_STOP);
+  ASSERT_EQ(getEventStateFromWaypoint(), autoware_msgs::WaypointState::TYPE_EVENT_POSITION_STOP)
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::TYPE_EVENT_POSITION_STOP;
 
-  test_obj_.setEventState(20, autoware_msgs::WaypointState::TYPE_EVENT_BUS_STOP);
-  ASSERT_EQ(test_obj_.getEventStateFromWaypoint(),
-            autoware_msgs::WaypointState::TYPE_EVENT_BUS_STOP)
-      << "Could not get the expected state"
-      << "It should be " << autoware_msgs::WaypointState::TYPE_EVENT_BUS_STOP;
+  setEventState(20, autoware_msgs::WaypointState::TYPE_EVENT_BUS_STOP);
+  ASSERT_EQ(getEventStateFromWaypoint(), autoware_msgs::WaypointState::TYPE_EVENT_BUS_STOP)
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::TYPE_EVENT_BUS_STOP;
 
-  test_obj_.setEventState(20, autoware_msgs::WaypointState::TYPE_EVENT_PARKING);
-  ASSERT_EQ(test_obj_.getEventStateFromWaypoint(),
-            autoware_msgs::WaypointState::TYPE_EVENT_PARKING)
-      << "Could not get the expected state"
-      << "It should be " << autoware_msgs::WaypointState::TYPE_EVENT_PARKING;
+  setEventState(20, autoware_msgs::WaypointState::TYPE_EVENT_PARKING);
+  ASSERT_EQ(getEventStateFromWaypoint(), autoware_msgs::WaypointState::TYPE_EVENT_PARKING)
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::TYPE_EVENT_PARKING;
 }
 
-TEST_F(TestSuite, getStopSignStateFromWaypoint) {
-	test_obj_.setCurrentPose(0, 0, 0);
-	test_obj_.setCurrentVelocity(10.0);
+TEST_F(TestSuite, getStopSignStateFromWaypoint)
+{
+  setCurrentPose(0, 0, 0);
+  setCurrentVelocity(10.0);
 
-	test_obj_.createFinalWaypoints();
-  std::pair<uint8_t, int> ret1 = test_obj_.getStopSignStateFromWaypoint();
+  createFinalWaypoints();
+  std::pair<uint8_t, int> ret1 = getStopSignStateFromWaypoint();
   ASSERT_EQ(ret1.first, autoware_msgs::WaypointState::NULLSTATE)
-      << "Could not get the expected state"
-      << "It should be " << autoware_msgs::WaypointState::NULLSTATE;
-  ASSERT_EQ(ret1.second, -1) << "Could not get the expected state"
-                             << "It should be " << -1;
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::NULLSTATE;
+  ASSERT_EQ(ret1.second, -1)
+    << "Could not get the expected state"
+    << "It should be "
+    << -1;
 
-  test_obj_.setStopState(20, autoware_msgs::WaypointState::TYPE_STOPLINE);
-  std::pair<uint8_t, int> ret2 = test_obj_.getStopSignStateFromWaypoint();
+  setStopState(20, autoware_msgs::WaypointState::TYPE_STOPLINE);
+  std::pair<uint8_t, int> ret2 = getStopSignStateFromWaypoint();
   ASSERT_EQ(ret2.first, autoware_msgs::WaypointState::TYPE_STOPLINE)
-      << "Could not get the expected state"
-      << "It should be " << autoware_msgs::WaypointState::TYPE_STOPLINE;
-  ASSERT_EQ(ret2.second, 20) << "Could not get the expected state"
-                             << "It should be " << 20;
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::TYPE_STOPLINE;
+  ASSERT_EQ(ret2.second, 20)
+    << "Could not get the expected state"
+    << "It should be "
+    << 20;
 
-  test_obj_.setStopState(20, autoware_msgs::WaypointState::TYPE_STOP);
-  std::pair<uint8_t, int> ret3 = test_obj_.getStopSignStateFromWaypoint();
+  setStopState(20, autoware_msgs::WaypointState::TYPE_STOP);
+  std::pair<uint8_t, int> ret3 = getStopSignStateFromWaypoint();
   ASSERT_EQ(ret3.first, autoware_msgs::WaypointState::TYPE_STOP)
-      << "Could not get the expected state"
-      << "It should be " << autoware_msgs::WaypointState::TYPE_STOP;
-  ASSERT_EQ(ret3.second, 20) << "Could not get the expected state"
-                             << "It should be " << 20;
+    << "Could not get the expected state"
+    << "It should be "
+    << autoware_msgs::WaypointState::TYPE_STOP;
+  ASSERT_EQ(ret3.second, 20)
+    << "Could not get the expected state"
+    << "It should be "
+    << 20;
 }
 
 } // namespace decision_maker
