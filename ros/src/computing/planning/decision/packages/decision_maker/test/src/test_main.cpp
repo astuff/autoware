ee441b69a0c106f27f2e1b1a5835738bc0cb6472
Azumi SUZUKI <azumi.suzuki@tier4.jp>
2019-02-05 11:12:59 +0900

    add test of decision_maker

--- ros/src/computing/planning/decision/packages/decision_maker/test/src/test_main.cpp
+++ ros/src/computing/planning/decision/packages/decision_maker/test/src/test_main.cpp
@@ -14,17 +14,19 @@
  * limitations under the License.
  */
 
-#include <gtest/gtest.h>
 #include <ros/ros.h>
+#include <gtest/gtest.h>
 
-class TestSuite : public ::testing::Test {
+class TestSuite: public ::testing::Test {
 public:
-  TestSuite() {}
-  ~TestSuite() {}
+	TestSuite(){}
+	~TestSuite(){}
 };
 
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "DecisionMakerTestNode");
-  return RUN_ALL_TESTS();
+int main(int argc, char **argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	ros::init(argc, argv, "DecisionMakerTestNode");
+	// ros::NodeHandle nh_;
+	return RUN_ALL_TESTS();
 }
--- ros/src/computing/planning/decision/packages/decision_maker/test/src/test_node_decision.cpp
+++ ros/src/computing/planning/decision/packages/decision_maker/test/src/test_node_decision.cpp
@@ -14,80 +14,97 @@
  * limitations under the License.
  */
 
-#include <gtest/gtest.h>
 #include <ros/ros.h>
+#include <gtest/gtest.h>
 
-#include "amathutils_lib/amathutils.hpp"
 #include "decision_maker_node.hpp"
+#include "amathutils_lib/amathutils.hpp"
 
-#include "test_class.hpp"
-
-namespace decision_maker {
+namespace decision_maker
+{
 
 class TestSuite : public ::testing::Test {
 public:
-  TestSuite() {}
-  ~TestSuite() {}
-
-  TestClass test_obj_;
+  TestSuite(){}
+  ~TestSuite(){}
 
 protected:
-  virtual void SetUp() {
+  virtual void SetUp()
+  {
     int argc;
-    char **argv;
-    test_obj_.dmn = new DecisionMakerNode(argc, argv);
+    char** argv;
+    dmn = new DecisionMakerNode(argc, argv);
   };
-  virtual void TearDown() { delete test_obj_.dmn; };
+  virtual void TearDown()
+  {
+    delete dmn;
+  };
+
+  DecisionMakerNode* dmn;
+
+  void createFinalWaypoints()
+  {
+    autoware_msgs::Lane final_lane;
+    for (int idx = 0; idx < 100; idx++)
+    {
+      static autoware_msgs::Waypoint wp;
+      wp.gid = idx;
+      wp.lid = idx;
+      wp.pose.pose.position.x = 0.0 + (double)idx;
+      wp.pose.pose.position.y = 0.0;
+      wp.pose.pose.position.z = 0.0;
+      wp.twist.twist.linear.x = 5.0;
+      wp.twist.twist.angular.z = 0.0;
+
+      final_lane.waypoints.push_back(wp);
+    }
+
+    dmn->current_status_.finalwaypoints = final_lane;
+  }
+
+  void setCurrentPose(double x, double y, double yaw)
+  {
+    geometry_msgs::Pose current_pose;
+    current_pose.position.x = x;
+    current_pose.position.y = y;
+    tf::Quaternion quaternion = tf::createQuaternionFromRPY(0, 0, yaw);
+    quaternionTFToMsg(quaternion, current_pose.orientation);
+
+    dmn->current_status_.pose = current_pose;
+  }
+
+  void setCurrentVelocity(double vel)
+  {
+    dmn->current_status_.velocity = vel;
+  }
+
+  bool isArrivedGoal() { return dmn->isArrivedGoal(); }
 };
 
-TEST_F(TestSuite, isLocalizationConvergence) {
-  geometry_msgs::Point pointA, pointB;
-  pointA.x = 10.0;
-  pointA.y = 10.0;
-  pointA.z = 0.0;
-  pointB.x = 10.0;
-  pointB.y = 12.0;
-  pointB.z = 0.0;
+TEST_F(TestSuite, isArrivedGoal)
+{
+  createFinalWaypoints();
 
-  for (int i=0; i<10; i++)
-  {
-    ASSERT_FALSE(test_obj_.isLocalizationConvergence(pointA)) << "It should be false";
-  }
-  ASSERT_TRUE(test_obj_.isLocalizationConvergence(pointA)) << "It should be true";
-
-  for (int i=0; i<5; i++)
-  {
-    ASSERT_TRUE(test_obj_.isLocalizationConvergence(pointA)) << "It should be true";
-    ASSERT_TRUE(test_obj_.isLocalizationConvergence(pointB)) << "It should be true";
-  }
-  ASSERT_TRUE(test_obj_.isLocalizationConvergence(pointA)) << "It should be true";
-
-  pointB.y = 12.1;
-  ASSERT_FALSE(test_obj_.isLocalizationConvergence(pointB)) << "It should be true";
-}
-
-TEST_F(TestSuite, isArrivedGoal) {
-	test_obj_.createFinalWaypoints();
-
-	test_obj_.setCurrentPose(100, 0, 0);
-	test_obj_.setCurrentVelocity(0.0);
-  ASSERT_TRUE(test_obj_.isArrivedGoal()) << "Current pose is outside the target range."
+  setCurrentPose(100, 0, 0);
+  setCurrentVelocity(0.0);
+  ASSERT_TRUE(isArrivedGoal()) << "Current pose is outside the target range."
                                << "It should be true";
 
-  test_obj_.setCurrentPose(100, 0, 0);
-  test_obj_.setCurrentVelocity(3.0);
-  ASSERT_FALSE(test_obj_.isArrivedGoal()) << "Current pose is inside the target range."
+  setCurrentPose(100, 0, 0);
+  setCurrentVelocity(3.0);
+  ASSERT_FALSE(isArrivedGoal()) << "Current pose is inside the target range."
                                 << "It should be false";
 
-  test_obj_.setCurrentPose(90, 0, 0);
-  test_obj_.setCurrentVelocity(0.0);
-  ASSERT_FALSE(test_obj_.isArrivedGoal()) << "Current pose is inside the target range."
+  setCurrentPose(90, 0, 0);
+  setCurrentVelocity(0.0);
+  ASSERT_FALSE(isArrivedGoal()) << "Current pose is inside the target range."
                                 << "It should be false";
 
-  test_obj_.setCurrentPose(90, 0, 0);
-  test_obj_.setCurrentVelocity(3.0);
-  ASSERT_FALSE(test_obj_.isArrivedGoal()) << "Current pose is inside the target range."
+  setCurrentPose(90, 0, 0);
+  setCurrentVelocity(3.0);
+  ASSERT_FALSE(isArrivedGoal()) << "Current pose is inside the target range."
                                 << "It should be false";
+
 }
 
 } // namespace decision_maker
